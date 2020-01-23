d832ac3e9bbcc1fd94433aa887329de4ab79dce3
akairen <jimequeen@outlook.jp>
2020-01-23 16:11:39 +0900

    cssいじった

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,10 +22,8 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(
-      content: params[:content],
-      type: params[:type],
-      time: params[:time],
-      user_id: @current_user.id
+      params.permit(:content,:type,:time).
+      mergeg(user_id: @current_user.id)
     )
     if @post.save
       redirect_to("/users/#{@current_user.id}")
