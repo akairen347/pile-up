860b1fbadac1a492579cd17d133a1bf996c92147
akairen <jimequeen@outlook.jp>
2020-03-18 09:56:29 +0900

    デフォルト試し

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET'], callback_url: "https://pile-up.herokuapp.com//auth/:provider/callback"
+    provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
   end
